6bedfeda005d64b8a65fe762e79f0b0d9061b997
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-22 02:22:03 +0800

    Complited cloud spawner and shuffler, need to fix a few small problems with it though. Some reason the clouds no longer seem to be visable, strange

--- Assets/Scripts/Cloud Collectors Script/CouldSpawner.cs
+++ Assets/Scripts/Cloud Collectors Script/CouldSpawner.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CouldSpawner : MonoBehaviour {
+public class CouldSpawner : MonoBehaviour
+{
 
     [SerializeField]
     private GameObject[] clouds; //Store our cloud objects
@@ -19,14 +20,71 @@ public class CouldSpawner : MonoBehaviour {
     //Postiion the player
     private GameObject player;
 
+    void Awake()
+    {
+        controlX = 0;
+        SetMinAndMax();
+        CreateClouds();
+    }
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    //Here we are making sure when we make our objects that they are not to far off screen for the palyer to see or use
+    void SetMinAndMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+
+    //Here we will randomise the postiton of our game objects
+    void Shuffle(GameObject[] arrayToShuffle)
+    {
+        for(int i = 0; i < arrayToShuffle.Length; i++)
+        {
+            GameObject temp = arrayToShuffle[i];
+            int random = Random.Range(i, arrayToShuffle.Length);
+            arrayToShuffle[i] = arrayToShuffle[random];
+            arrayToShuffle[random] = temp;
+        }
+    }
+
+    //I'll let you guess :D
+    void CreateClouds()
+    {
+        Shuffle(clouds);
+
+        float positionY = 0f;
+
+        //Forcing zig zar postiion
+        for(int i = 0; i < clouds.Length; i++)
+        {
+            Vector3 temp = clouds[i].transform.position;
+            temp.y = positionY;
+
+            if(controlX == 0)
+            {
+                temp.x = Random.Range(0.0f, maxX);
+                controlX = 1;
+            }
+            else if(controlX == 1)
+            {
+                temp.x = Random.Range(0.0f, minX);
+                controlX = 2;
+            }
+            else if (controlX == 2)
+            {
+                temp.x = Random.Range(1.0f, maxX);
+                controlX = 3;
+            }
+            else if (controlX == 3)
+            {
+                temp.x = Random.Range(-1.0f, minX);
+                controlX = 0;
+            }
+
+            lastCloudPositionY = positionY;
+            clouds[i].transform.position = temp;
+            positionY -= distanceBetweenClouds;
+        }
+    }
 } // CloudSpawner
