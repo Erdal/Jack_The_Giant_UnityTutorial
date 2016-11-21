ae8153f3f3ca7b6c967b7a14c360f80523c3a378
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-21 15:28:00 +0800

    Created our cloud collector and cloud spawner, created needed varables to finish the script in future and to store our clouds and colectables

---
+++ Assets/Scripts/Cloud Collectors Script/CloudCollector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudCollector : MonoBehaviour
+{
+    //Here we are going to decide what gets done when two objects with a collider colide together.
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        //Here i am setting any objects with the the two tags down below to false, so i am turning the objects off
+        if(target.tag == "Cloud" || target.tag == "Deadly")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Cloud Collectors Script/CouldSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CouldSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] clouds; //Store our cloud objects
+    //distence bweteen our objects
+    private float distanceBetweenClouds = 3f;
+    //help position our cloud objects
+    private float minX, maxX;
+    //Last clouds Y postion
+    private float lastCloudPositionY;
+    //Controll the X postiion of our clouds when spawning
+    private float controlX;
+
+    [SerializeField]
+    private GameObject[] collectables; //store collectable items
+    //Postiion the player
+    private GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+} // CloudSpawner
