636c10c501aea1b066e2e7ba594df8880e72cfc4
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-28 03:19:52 +0800

    I have created the code needed to make sure the player does not go out of bounds, but i now have the error of the player being stuck in one spot. This problem needs to be looked into as i am sure my code is correct, so it might be another script somehow eddecting it. I hope to have this fixed soon!

---
+++ Assets/Scripts/Player Scripts/PlayerBounds.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBounds : MonoBehaviour
+{
+    private float minX, maxX;
+
+	// Use this for initialization
+	void Start()
+    {
+        SetMinAndMax();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < minX)
+        {
+            Vector3 temp = transform.position;
+            temp.x = minX;
+            transform.position = temp;
+        }
+
+        if(transform.position.x > maxX)
+        {
+            Vector3 temp = transform.position;
+            temp.x = maxX;
+            transform.position = temp;
+        }
+
+    }
+
+    void SetMinAndMax()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        maxX = bounds.x;
+        minX = -bounds.x;
+    }
+}
