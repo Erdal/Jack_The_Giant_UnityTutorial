52ba4d22415835ddecc00e657ceef71ae84dd354
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-27 20:10:34 +0800

    Made camera slowly move its way down the screen, that way the player can actuelly see what the rest of the screen, camera has a max speed so that it can not go to fast for the player to keep up

---
+++ Assets/Scripts/Camera Scripts/CameraScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    private float speed = 1f;
+    private float acceleration = 0.2f;
+    private float maxSpeed = 3.2f;
+
+    [HideInInspector]
+    public bool moveCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        moveCamera = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(moveCamera)
+        {
+            MoveCamera();
+        }                                                                                                  
+	}
+
+    void MoveCamera()
+    {
+        Vector3 temp = transform.position;
+        float oldY = temp.y;
+        float newY = temp.y - (speed * Time.deltaTime);
+        temp.y = Mathf.Clamp(temp.y, oldY, newY);
+        transform.position = temp;
+        speed += acceleration * Time.deltaTime;
+
+        if(speed > maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+    }
+}
