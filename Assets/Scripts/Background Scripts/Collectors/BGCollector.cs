54af9833f67e074ed85c591399b99ed27870539a
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-28 02:51:16 +0800

    Created background collector and spawner. So what happens now is i use the background collector to de-activate backgrounds at the top of the screen that the user goes past. Then the background spawner to re-activate those same backgrounds under the player so that the player thinks its an endless map, when really were making just moving pieces around as the player goes down.

---
+++ Assets/Scripts/Background Scripts/Collectors/BGCollector.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGCollector : MonoBehaviour {
+    //Here we are going to turn off any backgrounds that our BGCollector collides with
+	void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Background")
+        {
+            target.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Background Scripts/Collectors/BGSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGSpawner : MonoBehaviour {
+
+    //Array of background images
+    private GameObject[] backgrounds;
+    private float lastY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetBackgroundAndSetLastY();
+	}
+	
+    //All in the title
+	void GetBackgroundAndSetLastY()
+    {
+        backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        lastY = backgrounds[0].transform.position.y;
+
+        for(int i = 1; i < backgrounds.Length; i++)
+        {
+            if(lastY > backgrounds[i].transform.position.y)
+            {
+                lastY = backgrounds[i].transform.position.y;
+            }
+        }
+    }
+
+    //Here we want to re-activate our de-activated backgrounds and place them after the last background 
+    //so that we may create the impression of an infinite background
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Background")
+        {
+            if(target.transform.position.y == lastY)
+            {
+                Vector3 temp = target.transform.position;
+                float height = ((BoxCollider2D)target).size.y;
+
+                for(int i = 0; i < backgrounds.Length; i++)
+                {
+                    if (!backgrounds[i].activeInHierarchy)
+                    {
+                        temp.y -= height;
+                        lastY = temp.y;
+
+                        backgrounds[i].transform.position = temp;
+                        backgrounds[i].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
