116b9a132600d5bdd8329b7b0c71c1bd12889ed8
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-30 03:17:30 +0800

    Just created my GameManager and used something called the Singleton Pattern i believe, it is amazing. there is still a lot of script i need to write for my GameManager, but i need sleep for now

---
+++ Assets/Scripts/Game Controllers/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public bool gameStartedFromMainMenu, gameRestartedAfterPlayerDied;
+
+	void Awake()
+    {
+        MakeSingleton();
+	}
+	
+	void MakeSingleton()
+    { //Here we are making sure that this class never gets deleted when we leave the original scene it is made in
+      //We are also making sure that when we return to this scene we do not create the same class again.
+        if(instance != null)
+        {
+            Destroy(gameObject); //destroy object
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); //Dont destroy object
+        }
+    }
+}
