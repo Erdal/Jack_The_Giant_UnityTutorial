d45981ff98c7b4bf1fc52ea6bcca9bf9cbf0c289
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-30 02:03:51 +0800

    Coded pause button, resume button and quit button, as well as a few small things here and there

---
+++ Assets/Scripts/Game Controllers/GamePlayController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamePlayController : MonoBehaviour
+{
+    //A varable of the class
+    public static GamePlayController instance;
+
+    [SerializeField]
+    private Text scoreText, coinText, lifeText; //So we can effect all out text elements on the UI
+
+    [SerializeField]
+    private GameObject pausePanel; //So we can effect our pause panel
+
+    // Use this for initialization
+    void Awake()
+    {
+        MakeInstance();
+	}
+	
+	void MakeInstance()
+    {
+        //Setting up the varable so i can use this class perfectly with just GamePlayController.instance when i am in other classes
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PauseTheGame()
+    {
+        Time.timeScale = 0f; //This keeps everything going
+        pausePanel.SetActive(true); //Shows the pause panel to the user
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; //This keeps everything going
+        pausePanel.SetActive(false); //Turns pause panel off
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f; //This keeps everything going
+        SceneManager.LoadScene("MainMenu"); //Load up the main menu
+    }
+}
