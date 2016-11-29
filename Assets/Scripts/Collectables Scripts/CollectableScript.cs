3289244d8c441c93dcbd0ae4621fb82040aa19f1
Erdal Erkilic <erdal.mybooks@hotmail.com>
2016-11-29 15:42:03 +0800

    Created our collectables, filled them in an array. Created collectableScript and attached to our collectables. Still needs a bit more code to be fully complete

---
+++ Assets/Scripts/Collectables Scripts/CollectableScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableScript : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Invoke("DestroyCollectable", 6f);
+    }
+
+	void DestroyCollectable()
+    {
+        gameObject.SetActive(false);
+    }
+}
